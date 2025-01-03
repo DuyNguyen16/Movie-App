d042f2e0cb23a2d34266437de2a4871ff1cdcaa2
DuyNguyen16 <khanhduyn161@gmail.com>
2025-01-03 20:54:36 +1100

    upd

--- api/api.tsx
+++ api/api.tsx
@@ -1,6 +1,4 @@
 
-export const apiKey = import.meta.env.VITE_API_KEY;
+export const apiKey = import.meta.env.VITE_apiKey;
 export const YOUTUBE_DATA_API = import.meta.env.VITE_YOUTUBE_DATA_API;
-export const firebaseAPIKey = import.meta.env.VITE_FIREBASE_API_KEY;
-
-console.log(firebaseAPIKey)
+export const firebaseAPIKey = import.meta.env.VITE_firebaseAPIKey;
