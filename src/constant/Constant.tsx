c5cd266ce359620232ccf050f30a0ad48517c7af
DuyNguyen16 <khanhduyn161@gmail.com>
2025-01-03 21:06:32 +1100

    adwa

--- src/constant/Constant.tsx
+++ src/constant/Constant.tsx
@@ -2,7 +2,7 @@ import { apiKey } from '../../api/api';
 import { User } from 'firebase/auth';
 import React from 'react';
 
-export const API_URL = `http://www.omdbapi.com/?apikey=${apiKey}`;
+export const API_URL = `https://www.omdbapi.com/?apikey=${apiKey}`;
 
 export interface UserContext {
     user: User | null; // user can be a User object or null
